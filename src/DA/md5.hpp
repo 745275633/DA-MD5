a0ed96c63a2cd57547b3f362bb9e9f6ca7096b40
745275633 <DAStudioDirector@gmail.com>
2017-08-14 17:07:47 +0800

    code style

--- src/DA/md5.hpp
+++ src/DA/md5.hpp
@@ -1,102 +1,107 @@
 #ifndef DA_MD5_HPP
 #define DA_MD5_HPP
 
-#include <string>
-#include <iostream>
 #include <array>
-#include <vector>
-#include <functional>
 #include <boost/cstdint.hpp>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
 
 namespace DA
 {
 namespace MD5
 {
-typedef boost::uint8_t byte;
-typedef boost::uint32_t uint32;
+using byte = boost::uint8_t;
+using uint32 = boost::uint32_t;
+
 struct data
 {
-	std::array<byte, 64> padding{{0x80}};
-	std::array<char, 16> hex_number
-	{{
-		'0', '1', '2', '3',
-		'4', '5', '6', '7',
-		'8', '9', 'a', 'b',
-		'c', 'd', 'e', 'f'
-	}};
-	std::array<uint32, 4> state
-	{{0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476}};
+  std::array<byte, 64> padding{{0x80}};
+  std::array<char, 16> hex_number
+  {{
+     '0', '1', '2', '3',
+     '4', '5', '6', '7',
+     '8', '9', 'a', 'b',
+     'c', 'd', 'e', 'f'
+   }};
+  std::array<uint32, 4> state
+  {{0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476}};
 
-	std::function<uint32(uint32, uint32, uint32)> f = 
-		[] (uint32 x, uint32 y, uint32 z) -> uint32
-		{
-			return (x & y) | (~x & z);
-		};
-	std::function<uint32(uint32, uint32, uint32)> g = 
-		[] (uint32 x, uint32 y, uint32 z) -> uint32
-		{
-			return (x & z) | (y & ~z);
-		};
-	std::function<uint32(uint32, uint32, uint32)> h = 
-		[] (uint32 x, uint32 y, uint32 z) -> uint32
-		{
-			return x ^ y ^ z;
-		};
-	std::function<uint32(uint32, uint32, uint32)> i = 
-		[] (uint32 x, uint32 y, uint32 z) -> uint32
-		{
-			return y ^ (x | ~z);
-		};
+  std::function<uint32(uint32, uint32, uint32)> f =
+    [] (uint32 x, uint32 y, uint32 z) -> uint32
+    {
+      return (x & y) | (~x & z);
+    };
+  std::function<uint32(uint32, uint32, uint32)> g =
+    [] (uint32 x, uint32 y, uint32 z) -> uint32
+    {
+      return (x & z) | (y & ~z);
+    };
+  std::function<uint32(uint32, uint32, uint32)> h =
+    [] (uint32 x, uint32 y, uint32 z) -> uint32
+    {
+      return x ^ y ^ z;
+    };
+  std::function<uint32(uint32, uint32, uint32)> i =
+    [] (uint32 x, uint32 y, uint32 z) -> uint32
+    {
+      return y ^ (x | ~z);
+    };
 
-	std::function<uint32(uint32, uint32)> rotate_left =
-		[] (uint32 x, uint32 n) -> uint32
-		{
-			return (x << n) | (x >> (32 - n));
-		};
-	// std::array<std::array<uint32, 4>, 4> s = {{
-		// {{ 7, 12, 17, 22}},
-		// {{ 5,  9, 14, 20}},
-		// {{ 4, 11, 16, 23}},
-		// {{ 6, 10, 15, 21}}
-	// }};
-	// std::array<std::array<uint32, 4>, 4> s = {{
-		// {{ 7,  5,  4,  6}},
-		// {{12,  9, 11, 10}},
-		// {{17, 14, 16, 15}},
-		// {{22, 20, 23, 21}}
-	// }};
+  std::function<uint32(uint32, uint32)> rotate_left =
+    [] (uint32 x, uint32 n) -> uint32
+    {
+      return (x << n) | (x >> (32 - n));
+    };
+  // std::array<std::array<uint32, 4>, 4> s = {{
+    // {{ 7, 12, 17, 22}},
+    // {{ 5,  9, 14, 20}},
+    // {{ 4, 11, 16, 23}},
+    // {{ 6, 10, 15, 21}}
+  // }};
+  // std::array<std::array<uint32, 4>, 4> s = {{
+    // {{ 7,  5,  4,  6}},
+    // {{12,  9, 11, 10}},
+    // {{17, 14, 16, 15}},
+    // {{22, 20, 23, 21}}
+  // }};
 };
 class MD5
 {
 private:
-	const data _data;
-	std::array<uint32, 4> _state;
-	std::array<uint32, 2> _count;
-	std::array<byte, 64> _buffer;
-	std::array<byte, 16> _digest;
-	bool _finished;
+  data _data;
+  std::array<uint32, 4> _state;
+  std::array<uint32, 2> _count;
+  std::array<byte, 64> _buffer;
+  std::array<byte, 16> _digest;
+  bool _finished;
 
-	enum { BUFFER_SIZE = 1024 };
-	void final();
-	void transform(const std::array<byte, 64> & block);
-	std::vector<byte> encode(const std::vector<uint32> input);
-	std::vector<uint32> decode(const std::vector<byte> & input);
-	std::string bytesToHexString(const byte* input, size_t length);
-	std::string bytesToHexString(std::vector<byte> input);
+  const static int BUFFER_SIZE = 1024;
+  void final();
+  void transform(const std::array<byte, 64>& block);
+  std::vector<byte> encode(const std::vector<uint32>& input);
+  std::vector<uint32> decode(const std::vector<byte>& input);
+  std::string bytesToHexString(const byte* input, size_t length);
+  std::string bytesToHexString(std::vector<byte> input);
 
 public:
-	MD5();
-	MD5(const struct data & a);
-	MD5(const MD5&) = delete;
-	MD5& operator=(const MD5&) = delete;
-	void update(const std::string& str);
-	void update(std::istream& in);
-	void update(const std::vector<byte> & input);
-	const std::array<byte, 16> digest();
-	std::string toString();
-	void reset();
+  MD5();
+  explicit MD5(data a);
+  ~MD5() = default;
+  MD5(const MD5&) = default;
+  MD5& operator=(const MD5&) = default;
+  MD5(MD5&&) noexcept(false) /*For clang-tidy*/ = default;
+  MD5& operator=(MD5&&) noexcept(false) = default;
+  void update(const std::string& str);
+  void update(std::istream& in);
+  void update(const std::vector<byte>& input);
+  const std::array<byte, 16> digest();
+  std::string toString();
+  void reset();
 };
-} /* MD5 */
-} /* DA */
+
+} // namespace MD5
+} // namespace DA
 
 #endif /* DA_MD5_HPP */
--- xmake.lua
+++ xmake.lua
@@ -2,17 +2,17 @@ set_xmakever("2.1.3")
 set_languages("gnu++14")
 
 target("DA-MD5")
-	set_kind("shared")
-	add_files("src/*.cpp") 
-	add_headers("src/(DA/*.hpp)")
-	before_build(function (target)
-		if not os.exists("/usr/include/boost") then
-			if io.stderr == nil then
-				io.stderr = io.open("/dev/stderr", "a")
-			end
-			io.stderr:print("Need boost");
-			io.stderr:print("Install it with package manager")
-			io.stderr:print("Or http://www.boost.org/")
-			os.exit(1);
-		end
-	end)
+  set_kind("shared")
+  add_files("src/*.cpp") 
+  add_headers("src/(DA/*.hpp)")
+  before_build(function (target)
+    if not os.exists("/usr/include/boost") then
+      if io.stderr == nil then
+        io.stderr = io.open("/dev/stderr", "a")
+      end
+      io.stderr:print("Need boost");
+      io.stderr:print("Install it with package manager")
+      io.stderr:print("Or http://www.boost.org/")
+      os.exit(1);
+    end
+  end)
